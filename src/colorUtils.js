4ad02e76f4b25ba29adb142dfeb1cbee5152ea48
Zola <zolamcadie@gmail.com>
2019-08-22 11:43:20 -0400

    Add color parser functions

--- src/colorUtils.js
+++ src/colorUtils.js
@@ -1,3 +1,37 @@
+// only works with 7 character hex codes, i.e. #007BFF
+const parseHex = color => color.slice(1).match(/.{2}/g).map(c => parseInt(c, 16))
+
+// only works with rgb, does not support alpha channel
+const parseRGB = color => color.slice(4, -1).split(", ").map(c => parseInt(c, 10))
+
+//////////////////////////////
+//
+//    TO DO
+//    ‾‾‾‾‾
+//    1. Add alpha channel support to parseHex and parseRGB
+//
+//    2. Add support for 3 digit hex shorthand (and 4 digit with alpha channel support)
+//
+//////////////////////////////
+
+const actions = {
+  hex: parseHex,
+  rgb: parseRGB,
+  unknown: c => c
+}
+
+const getRGB = color => {
+  const type = color[0] === "#"
+    ? "hex"
+    : color.slice(0, 3).toLowerCase() === "rgb"
+      ? "rgb"
+      : "unknown"
+  return {
+    type,
+    color: actions[type](color)
+  }
+}
+
 const rgblogend = (c0, c1, p) => {
   const v = s => _ => s * parseInt(_) ** 2;
   const C = (_, s) =>
@@ -13,10 +47,7 @@ const rgblogend = (c0, c1, p) => {
 };
 
 const lighten = (hex, adj = 0.1) => {
-  const clrArr = hex
-    .slice(1)
-    .match(/.{2}/g)
-    .map(clr => parseInt(clr, 16));
+  const clrArr = getRGB(hex).color
   const adjusted = clrArr.map(clr => clr + Math.round((255 - clr) * adj));
   const strArr = adjusted.map(a => `${a < 16 ? "0" : ""}${a.toString(16)}`);
   return `#${strArr.join("")}`;
