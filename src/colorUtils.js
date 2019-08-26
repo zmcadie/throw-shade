0ff8a76059a3f8da120adbc77db43bdea5db13ec
Zola <zolamcadie@gmail.com>
2019-08-26 09:10:15 -0400

    Refactor blend functions and add hsl support

--- src/colorUtils.js
+++ src/colorUtils.js
@@ -4,11 +4,15 @@ const pad0 = str => `${str.length === 1 ? '0' : ''}${str}`
 const parseHex = color => color.slice(1).match(/.{2}/g).map(c => parseInt(c, 16))
 
 // only works with rgb, does not support alpha channel
-const parseRGB = color => color.slice(4, -1).split(", ").map(c => parseInt(c, 10))
+const parseRGB = color => color.match(/\d+/g).map(n => parseInt(n, 10))
+
+const parseHSL = parseRGB
 
 const arrToRGB = arr => `rgb(${arr.join(", ")})`
 
 const arrToHex = arr => `#${arr.map(n => pad0(n.toString(16))).join("")}`
+
+const arrToHsl = arr => `hsl(${arr[0]}, ${arr[1]}%, ${arr[2]}%)`
 
 //////////////////////////////
 //
@@ -20,56 +24,54 @@ const arrToHex = arr => `#${arr.map(n => pad0(n.toString(16))).join("")}`
 //
 //////////////////////////////
 
-const parseAction = {
+const parse = {
   hex: parseHex,
   rgb: parseRGB,
+  hsl: parseHSL,
   unknown: c => c
 }
 
 const toStrAction = {
   hex: arrToHex,
   rgb: arrToRGB,
+  hsl: arrToHsl,
   unknown: c => c
 }
 
 const getColorArray = color => {
+  const func = color.slice(0, 3).toLowerCase()
   const type = color[0] === "#"
     ? "hex"
-    : color.slice(0, 3).toLowerCase() === "rgb"
-      ? "rgb"
+    : ["rgb", "hsl"].includes(func)
+      ? func
       : "unknown"
-  return [ type, parseAction[type](color) ]
+  return [ type, parse[type](color) ]
 }
+
+const mixNum = (num1, num2, adj) => num1 + ((num2 - num1) * adj)
+const blendArr = (arr1, arr2, adj) => arr1.map((num, i) => Math.trunc(mixNum(num, arr2[i], adj)))
 
 const logBlend = (color1, color2, adj) => {
-  const squareShade = shade => color => shade * (color ** 2)
-  const squareColor = (clr, shd) => clr.map(squareShade(shd))
-  const color1Squared = squareColor(color1, 1 - adj)
-  const color2Squared = squareColor(color2, adj)
-  const blendSquareRoot = (clr, i) => Math.trunc((clr + color2Squared[i]) ** 0.5)
-  return color1Squared.map(blendSquareRoot)
+  const color1Squared = color1.map(clr => clr ** 2)
+  const color2Squared = color2.map(clr => clr ** 2)
+  return blendArr(color1Squared, color2Squared, adj).map(clr => Math.trunc(clr ** 0.5))
 }
 
-const linearBlend = (color1, color2, adj) => {
-  const blend = (clr, i) => clr + Math.trunc((color2[i] - clr) * adj)
-  return color1.map(blend)
-}
+const blenders = { log: logBlend, linear: blendArr }
 
-const blenders = { log: logBlend, linear: linearBlend }
-
-const blend = (color1, color2, adj = 0.5, log = true) => {
+const blendColors = (color1, color2, adj = 0.5, log = true) => {
   const [ type1, colorArr1 ] = getColorArray(color1)
   const [ , colorArr2 ] = getColorArray(color2)
-  const blended = blenders[log ? 'log' : 'linear'](colorArr1, colorArr2, adj)
+  const blended = blenders[log && type1 !== "hsl" ? 'log' : 'linear'](colorArr1, colorArr2, adj)
   return toStrAction[type1](blended)
 }
 
-const lighten = (color, adj = 0.1, log = true) => blend(color, "#FFFFFF", adj, log)
+const lighten = (color, adj = 0.1, log = true) => blendColors(color, "#FFFFFF", adj, log)
 
-const darken = (color, adj = 0.1, log = true) => blend(color, "#000000", adj, log)
+const darken = (color, adj = 0.1, log = true) => blendColors(color, "#000000", adj, log)
 
 export {
-  blend,
+  blendColors as blend,
   lighten,
   darken
 }
